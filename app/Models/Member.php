fe5b0ff7cdd449716038eb0838971168ca4e093a
fahmi <amixcustomlinux@gmail.com>
2025-05-15 16:17:16 +0700

    [DEV] Member: DB Migration, Model, Seeder

---
+++ app/Models/Member.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Member extends Model
+{
+    /** @use HasFactory<\Database\Factories\MemberFactory> */
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'id',
+        'full_name',
+        'birthdate',
+        'address',
+        'created_at',
+        'updated_at'
+    ];
+}
