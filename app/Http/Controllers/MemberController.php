158b40b4720fa6a9613879d1fa8ed6d73070a090
fahmi <amixcustomlinux@gmail.com>
2025-05-16 08:47:12 +0700

    [DEV] Member > Delete

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -85,6 +85,8 @@ class MemberController extends Controller
      */
     public function destroy(Member $member)
     {
-        //
+        $member->delete();
+        
+        return redirect()->route('members.index');
     }
 }
