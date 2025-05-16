55226aa3d605da9f304f7f03ec8b832cf1c54c82
fahmi <amixcustomlinux@gmail.com>
2025-05-16 09:52:18 +0700

    [DEV] Publisher > Delete

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -78,8 +78,10 @@ class PublisherController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Publisher $publisher)
     {
-        //
+        $publisher->delete();
+        
+        return redirect()->route('publishers.index');
     }
 }
